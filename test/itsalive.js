21a8d03c15f37ab5a8a4885a059102f1916801e7
Kelvin Masilamani <kmasila00@gmail.com>
2016-05-03 17:55:38 -0400

    first swtich

--- test/itsalive.js
+++ test/itsalive.js
@@ -1,1 +1,40 @@
-console.log('I said it salive!');
+var expect = require('chai').expect;
+var chai = require('chai');
+var spies = require('chai-spies');
+chai.use(spies);
+
+describe('testing suite', function(){
+	it('confirms basic math', function(){
+		expect(2+2).to.equal(4);
+	});
+});
+
+describe('check timeout', function(){
+	it('checking', function(done){
+		var start = new Date();
+		setTimeout( function (){
+			var duration  = new Date()- start;
+			expect(duration).to.be.closeTo(1000,50);
+			done();
+		}, 1000);
+	});
+});
+
+
+describe('spying', function(){
+
+	it('is it spying on stuff?', function(){
+		var a = [1,2,3,4,5];
+
+		function addTwo(num){
+			return num + 2;
+		}
+
+		var addTwo = chai.spy(addTwo);
+
+		a.forEach(addTwo);
+		expect(addTwo).to.have.been.called.exactly(a.length);
+	});
+});
+
+
